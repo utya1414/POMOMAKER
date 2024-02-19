15dd0e04162991f16363959b12106ea6270e8645
takusankai <j021328@gmail.com>
2024-02-19 17:52:31 +0900

    db操作apiの基礎を作成、挿入一覧が出来るようにフロントを変更

--- client/app/page.js
+++ client/app/page.js
@@ -2,11 +2,50 @@
 import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const URL = "http://127.0.0.1:8080/api/"
+
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("http://127.0.0.1:8080/api/home")
+    console.log(URL + "home")
+    fetch(URL + "home")
       .then((response) => response.json())
       .then((data) => setMessage(data.message));
-  }, []);
-  return <div>{message}</div>;
+  }, [])
+
+  const [input_data, setInputData] = useState("")
+  function handleEnter(e) {
+    if (e.key === "Enter") {
+      setInputData(e.target.value)
+      fetch(URL + "insert_user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "user_name": e.target.value })
+      })
+    }
+  }
+
+  const [users_data, setUsersData] = useState([])
+  function handleClick() {
+    fetch(URL + "get_users")
+      .then((response) => response.json())
+      .then((data) => setUsersData(data.users))
+  }
+
+  return (
+    <>
+      <h1>here is first page</h1>
+      <p>Message from server: {message}</p>
+      <br></br>
+      <p>↓に入力欄</p>
+      <input type="text" onKeyDown={handleEnter}></input>
+      <p>your input: {input_data}</p>
+      <br></br>
+      <button onClick={handleClick}>このボタンで一覧を取得</button>
+      <div>
+        Users data: {users_data.map((user, index) => <p key={index}>{user.user_name}</p>)}
+      </div>
+    </>
+  );
 }
--- server/server.py
+++ server/server.py
@@ -1,14 +1,49 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # app instance
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sample.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/dbname'
+# username、password、localhost、dbnameはそれぞれMySQLのユーザー名、パスワード、ホスト名、データベース名に置き換える
 
-# /api/home
+db = SQLAlchemy(app)
+
+# ユーザー情報テーブルを作成
+class User(db.Model):
+    uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(30))
+    create_day = db.Column(db.String(30))
+
+with app.app_context():
+    db.create_all()
+
+# 単にHello, World!を返却する
 @app.route('/api/home', methods=['GET'])
 def return_home():
     return jsonify({'message': 'Hello, World!'})
+
+# 受け取ったユーザ名をユーザー情報テーブルに登録する
+@app.route('/api/insert_user', methods=['POST'])
+def insert_user():
+    user_data = request.get_json()
+    today = datetime.now()
+    user = User(user_name=user_data['user_name'], create_day=today)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User added successfully'})
+
+# ユーザー情報テーブルの全データを取得する
+@app.route('/api/get_users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    user_list = []
+    for user in users:
+        user_list.append({'uid': user.uid, 'user_name': user.user_name, 'create_day': user.create_day})
+    return jsonify({'users': user_list})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
