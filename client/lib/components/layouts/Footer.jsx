ec46fb82bd1410bc957c0d72ac1152042ab7e81a
utya1414 <utya1414@gmail.com>
2024-02-23 23:02:30 +0900

    add: footerのダークモード対応

--- client/lib/components/layouts/Footer.jsx
+++ client/lib/components/layouts/Footer.jsx
@@ -3,18 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer style={
-        {
-          position: "fixed",
-          left: "0",
-          bottom: "0",
-          width: "100%",
-          textAlign: "center",
-          padding: "10px",
-          borderTop: "1px solid black" 
-        }
-      
-      }>Copyright © ○○ All Rights Reserverd.</footer>
+      <footer className="flex justify-center items-center py-6">
+        Copyright © ○○ All Rights Reserverd.
+      </footer>
     </>
   );
 };
