6ec573a909cfb3e5bf0e983b96618e7e3f18e477
takusankai <j021328@gmail.com>
2024-02-25 14:31:53 +0900

    user_idを使ったタイマーリクエストを追加

--- client/app/(Home)/chart/page.jsx
+++ client/app/(Home)/chart/page.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { GetAllTimerCardStats } from "@/api/timer";
 
-const page = () => {
-  return <div>chartpage</div>;
-};
+const AllTimers = () => {
+    const [timer_data, setUserData] = useState({});
 
-export default page;
+    useEffect(() => {
+        async function fetchData() {
+            const result = await GetAllTimerCardStats();
+            setUserData(result.data);
+            console.log("timer_data", result.data);
+        }
+        fetchData();
+    },[]);
+
+    return (
+        <div>
+            <h1>皆のタイマー</h1>
+            <h2>タイマー情報</h2>
+            {/* 以下にmapを使ってtimer_dataの全ての情報を出力する */}
+            {Object.keys(timer_data).map((key, index) => {
+                return (
+                    <div key={index}>
+                        <p>{key}: {timer_data[key]}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+export default AllTimers;
